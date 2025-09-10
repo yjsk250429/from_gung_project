931c61f7a235d1977e6f147042f9196e0a7717ac
burtsbees100 <kje_4227@naver.com>
2025-09-10 20:12:07 +0900

    OttMain

--- src/components/ui/button/Button.jsx
+++ src/components/ui/button/Button.jsx
@@ -1,26 +1,11 @@
 import './style.scss';
 
-const Button = ({
-  text = '확인',
-  width,
-  height,
-  onClick = () => {},
-  className = '',
-}) => {
-  const style = {
-    width: width || '150px',
-    height: height || '42px',
-  };
-
-  return (
-    <button
-      className={`button ${className}`}
-      style={style}
-      onClick={onClick}
-    >
-      {text}
-    </button>
-  );
+const Button = ({ text = '확인', onClick = () => {}, className = '' }) => {
+    return (
+        <button className={`button ${className}`} onClick={onClick}>
+            {text}
+        </button>
+    );
 };
 
 export default Button;

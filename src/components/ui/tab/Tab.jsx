d044d378ba68dcd91ae1707ee5cfe0aa24c0b7f4
yjsk250429 <yejin1046@gmail.com>
2025-09-10 19:43:33 +0900

    헤더 수정, ui 컴포넌트 작업중

--- src/components/ui/tab/Tab.jsx
+++ src/components/ui/tab/Tab.jsx
@@ -1,10 +1,18 @@
+import { useState } from 'react';
 import './style.scss';
 
 const Tab = ({ items = [], onClick = () => {} }) => {
+    const [activeTab, setActiveTab] = useState(0);
+    
+    const handleclick = (index) =>{
+        setActiveTab(index);
+        onClick(index);
+    };
+
   return (
     <ul className="tabs">
       {items.map((text, index) => (
-        <li key={index} onClick={() => onClick(index)}>
+        <li key={index} onClick={() => handleclick(index)} className={activeTab===index? 'on':''}>
           {text}
         </li>
       ))}

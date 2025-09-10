afef63069c281584a3d6ea761851d3571c588f2c
jju <ichie8699@gmail.com>
2025-09-10 20:54:03 +0900

    jm4

---
+++ src/components/ottDetail/OttDetailVisual.jsx
@@ -0,0 +1,68 @@
+import './style.scss';
+
+const OttDetailVisual = () => {
+    return (
+        <div class="detailvisual">
+            <div className="bg"></div>
+            <div className="inner">
+                <div className="left">
+                    <div className="title">
+                        <strong>회차</strong>
+                        <p>(총 18회)</p>
+                    </div>
+                    <div className="list">
+                        <div className="list contents1">
+                            <img src="/images/ott/ottdetail.png" alt="1회" />
+                            <strong>
+                                EP.1 <span>달빛연인</span>
+                            </strong>
+                            <div className="date">
+                                <p>59분</p>
+                                <p>|</p>
+                                <p>2016.08.22</p>
+                            </div>
+                        </div>
+                        <div className="list contents1">
+                            <img src="/images/ott/ottdetail.png" alt="1회" />
+                            <strong>
+                                EP.1 <span>달빛연인</span>
+                            </strong>
+                            <div className="date">
+                                <p>59분</p>
+                                <p>|</p>
+                                <p>2016.08.22</p>
+                            </div>
+                        </div>
+                        <div className="list contents1">
+                            <img src="/images/ott/ottdetail.png" alt="1회" />
+                            <strong>
+                                EP.1 <span>달빛연인</span>
+                            </strong>
+                            <div className="date">
+                                <p>59분</p>
+                                <p>|</p>
+                                <p>2016.08.22</p>
+                            </div>
+                        </div>
+                        <div className="list contents1">
+                            <img src="/images/ott/ottdetail.png" alt="1회" />
+                            <strong>
+                                EP.1 <span>달빛연인</span>
+                            </strong>
+                            <div className="date">
+                                <p>59분</p>
+                                <p>|</p>
+                                <p>2016.08.22</p>
+                            </div>
+                        </div>
+                        ß
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+//https://codesandbox.io/p/sandbox/y7gs6q?file=%2Fsrc%2FApp.jsx%3A22%2C29
+
+export default OttDetailVisual;
--- src/pages/ottDetail/index.jsx
+++ src/pages/ottDetail/index.jsx
@@ -1,8 +1,15 @@
+import OttDetailContents from "../../components/ottDetail/OttDetailContents";
+import OttDetailPeople from "../../components/ottDetail/OttDetailPeople";
+import OttDetailReview from "../../components/ottDetail/OttDetailReview";
+import OttDetailVisual from "../../components/ottDetail/OttDetailVisual";
 
 const OttDetail = () => {
     return (
         <div>
-            
+            <OttDetailVisual/>
+            <OttDetailPeople/>
+            <OttDetailReview/>
+            <OttDetailContents/>
         </div>
     );
 };

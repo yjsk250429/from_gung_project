c5df4205d4fc4b9d53f248f941b80f52e0bd1431
yjsk250429 <yejin1046@gmail.com>
2025-09-10 13:13:17 +0900

    헤더푸터 로고추가

--- src/common/footer/index.jsx
+++ src/common/footer/index.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
             </div>
             <div className="inner">
                 <div className="top">
-                <h2>하단로고</h2>
+                <h2><a href="#"><img src="/images/logo.png" alt="fromgung" /></a></h2>
                 <ul className="policy">
                     <li>개인정보처리방침</li>
                     <li>유튜브</li>
@@ -14,12 +14,12 @@ const Footer = () => {
                 </div>
                 <div className="bottom">
                     <ul className="info">
-                        <li>상호.   궁에서 온</li>
-                        <li>대표.   홍석균</li>
-                        <li>대표번호.   1544-1234</li>
-                        <li>주소.   서울 서초구 서초대로77길 41 대동2빌딩 9층 </li>
-                        <li>e-mail.   ghltkdlfma@ezen.co.kr</li>
-                        <li>사업자 번호.   130-12-35678</li>
+                        <li>상호. 궁에서 온</li>
+                        <li>대표. 홍석균</li>
+                        <li>대표번호. 1544-1234</li>
+                        <li>주소. 서울 서초구 서초대로77길 41 대동2빌딩 9층 </li>
+                        <li>e-mail. ghltkdlfma@ezen.co.kr</li>
+                        <li>사업자 번호. 130-12-35678</li>
                     </ul>
                     <p className="copy">FromGung ⓒ All rights reserved.</p>
                 </div>

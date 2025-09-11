5e15050dc74c7269548bd7f26de16adc5a286b13
yjsk250429 <yejin1046@gmail.com>
2025-09-11 21:14:41 +0900

    투어 리스트 필터 추가 중

--- src/pages/product/index.jsx
+++ src/pages/product/index.jsx
@@ -3,6 +3,9 @@ import TourClassTop from "../../components/tourClass/tourClassTop";
 import Button from "../../components/ui/button/Button";
 import Tab from "../../components/ui/tab/Tab";
 import './style.scss';
+import { IoIosList } from "react-icons/io";
+import { BiSortAlt2 } from "react-icons/bi";
+
 
 const TourClass = () => {
     const tabItems = ['전체', '서울', '인천/경기', '지방'];
@@ -13,6 +16,10 @@ const TourClass = () => {
             <div className="inner">
                 <TourClassTop/>
                 <Tab items={tabItems}/>
+                <ul className="sort">
+                    <li><i><IoIosList /></i>필터</li>
+                    <li><i><BiSortAlt2 /></i>정렬</li>
+                </ul>
                 <TourClassList/>
                 <p className="more">
                 <Button text="더보기" className="default"/>

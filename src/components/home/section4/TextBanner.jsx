56a032079b6f6b85e4e811274f40eb54e0dd3f9e
deaoeon <deaoeon1@gmail.com>
2025-09-11 16:07:09 +0900

    textbanner

--- src/components/home/section4/TextBanner.jsx
+++ src/components/home/section4/TextBanner.jsx
@@ -1,31 +1,42 @@
 import gsap from 'gsap';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 const topItems = ['LIKE NEVER BEFORE ,', 'UNFORGETTABLE ,', 'EXCLUSIVE ,'];
 
 const bottomItems = ['지금까지 몰랐던 한국 , ', '지금 경험해보세요 '];
 
 const TextBanner = () => {
-    const containerRef = useRef(null);
+    const textLeftRef = useRef(null);
+    const textRightRef = useRef(null);
 
     useEffect(() => {
-        const ctx = gsap.context(() => {
-            gsap.to('.s4_top-text', {
-                x: '-50%',
+        if (textLeftRef.current) {
+            gsap.set(textLeftRef.current, { xPercent: 0 });
+
+            gsap.to(textLeftRef.current, {
+                xPercent: -50,
+                repeat: -1,
+                duration: 15,
+                ease: 'linear',
+            });
+        }
+        if (textRightRef.current) {
+            gsap.set(textRightRef.current, { xPercent: -50 });
+
+            gsap.to(textRightRef.current, {
+                xPercent: 0,
+                repeat: -1,
                 duration: 10,
                 ease: 'linear',
-                repeat: -1,
             });
-        }, containerRef);
-
-        return () => ctx.revert(); // 모든 애니메이션 자동 정리
+        }
     }, []);
 
     return (
         <div className="section4-main">
             {/* 위쪽 텍스트 */}
             <div className="marquee-container">
-                <ul className="s4_top-text">
+                <ul className="s4_top-text" ref={textLeftRef}>
                     {/* {topItems.map((item, i) => (
                         <li key={i}>{item}</li>
                     ))} */}
@@ -36,10 +47,9 @@ const TextBanner = () => {
 
             {/* 아래쪽 텍스트 */}
             <div className="marquee-container">
-                <ul className="s4_bottom-text">
-                    {bottomItems.map((item, i) => (
-                        <li key={i}>{item}</li>
-                    ))}
+                <ul className="s4_bottom-text" ref={textRightRef}>
+                    <li>지금까지 몰랐던 한국 지금 경험해보세요</li>
+                    <li>지금까지 몰랐던 한국 지금 경험해보세요</li>
                 </ul>
             </div>
         </div>

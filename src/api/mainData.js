4a1249e2d4a0fdd28c68423c9d4f7db11b2d57f6
deaoeon <deaoeon1@gmail.com>
2025-09-11 13:16:37 +0900

    효과 추가

--- src/api/mainData.js
+++ src/api/mainData.js
@@ -1,10 +1,10 @@
 export default [
-  {id:1, img: '/images/con1_default.png', label: '덕수궁기행' },
-  {id:2, img: '/images/con1_default.png', label: '경복궁투어' },
-  {id:3, img: '/images/con1_default.png', label: '창덕궁산책' },
-  {id:4, img: '/images/con1_default.png', label: '종묘문화재' },
-  {id:5, img: '/images/con1_default.png', label: '한옥마을체험' },
-  {id:6, img: '/images/con1_default.png', label: '서울야경투어' },
-  {id:7, img: '/images/con1_default.png', label: '북촌골목길' },
-  {id:8, img: '/images/con1_default.png', label: '전통시장탐방' }
+    { id: 1, img: '/images/con1_default.png', label: '덕수궁기행', text: 'ㄱ' },
+    { id: 2, img: '/images/con1_default.png', label: '경복궁투어', text: 'ㄴ' },
+    { id: 3, img: '/images/con1_default.png', label: '창덕궁산책', text: 'ㄷ' },
+    { id: 4, img: '/images/con1_default.png', label: '종묘문화재', text: 'ㄹ' },
+    { id: 5, img: '/images/con1_default.png', label: '한옥마을체험', text: 'ㅁ' },
+    { id: 6, img: '/images/con1_default.png', label: '서울야경투어', text: 'ㅂ' },
+    { id: 7, img: '/images/con1_default.png', label: '북촌골목길', text: 'ㅅ' },
+    { id: 8, img: '/images/con1_default.png', label: '전통시장탐방', text: 'ㅇ' },
 ];

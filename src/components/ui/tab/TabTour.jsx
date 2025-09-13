80f03328355c3ac78f66e51d212fa4ac72a3359d
jju <ichie8699@gmail.com>
2025-09-14 00:58:23 +0900

    jm-0913-3

---
+++ src/components/ui/tab/TabTour.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import './styleTour.scss';
+
+const Tab = ({ items = [], onClick = () => {}, textColor, borderColor, height }) => {
+    const [activeTab, setActiveTab] = useState(0);
+    const handleclick = (index) => {
+        setActiveTab(index);
+        onClick(index);
+    };
+
+    return (
+        <ul className="tabs">
+            {items.map((text, index) => (
+                <li
+                    key={index}
+                    onClick={() => handleclick(index)}
+                    className={activeTab === index ? 'on' : ''}
+                >
+                    {text}
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+export default Tab;

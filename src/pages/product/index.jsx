6a14b5e1c23c945939cabd78468aeaf7acf99187
jju <ichie8699@gmail.com>
2025-09-14 00:58:23 +0900

    jm-0913-3

--- src/pages/product/index.jsx
+++ src/pages/product/index.jsx
@@ -1,11 +1,10 @@
-import TourClassList from "../../components/tourClass/TourClassList";
-import TourClassTop from "../../components/tourClass/TourClassTop";
-import Button from "../../components/ui/button/Button";
-import Tab from "../../components/ui/tab/Tab";
+import TourClassList from '../../components/tourClass/TourClassList';
+import TourClassTop from '../../components/tourClass/TourClassTop';
+import Button from '../../components/ui/button/Button';
+import Tab from '../../components/ui/tab/Tab';
 import './style.scss';
-import { IoIosList } from "react-icons/io";
-import { BiSortAlt2 } from "react-icons/bi";
-
+import { IoIosList } from 'react-icons/io';
+import { BiSortAlt2 } from 'react-icons/bi';
 
 const TourClass = () => {
     const tabItems = ['전체', '서울', '인천/경기', '지방'];
@@ -14,15 +13,25 @@ const TourClass = () => {
         <section className="tourclass">
             <div className="bg"></div>
             <div className="inner">
-                <TourClassTop/>
-                <Tab items={tabItems}/>
+                <TourClassTop />
+                <Tab items={tabItems} />
                 <ul className="sort">
-                    <li><i><IoIosList /></i>필터</li>
-                    <li><i><BiSortAlt2 /></i>정렬</li>
+                    <li>
+                        <i>
+                            <IoIosList />
+                        </i>
+                        필터
+                    </li>
+                    <li>
+                        <i>
+                            <BiSortAlt2 />
+                        </i>
+                        정렬
+                    </li>
                 </ul>
-                <TourClassList/>
+                <TourClassList />
                 <p className="more">
-                <Button text="더보기" className="default"/>
+                    <Button text="더보기" className="default" />
                 </p>
             </div>
         </section>

5b8757a9753fc0c7098210e95c2f2e576aa3e41e
burtsbees <kje_4227@naver.com>
2025-09-16 00:55:10 +0900

    KJE_tourclass_hover

--- src/components/tourClass/TourClassItem.jsx
+++ src/components/tourClass/TourClassItem.jsx
@@ -1,55 +1,79 @@
-import { FiHeart } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { FiHeart } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import { IoTimeOutline } from "react-icons/io5";
 
-const TourClassItem = ({ id, title, theme, description, period, price, img }) => {
-    return (
-        <li>
-            <Link to={`/tourclass/${id}`}>
-                <div className="img-wrap">
-                    <img src={img} alt={title} />
-                    <article className="tourclassInfo">
-                        <div className="tourclassThemes">
-                            {theme.map((item, index) => (
-                                <div
-                                    key={index}
-                                    className={
-                                        item === '역사'
-                                            ? 'tourclassTheme history'
-                                            : item === '예술'
-                                            ? 'tourclassTheme art'
-                                            : item === '힐링'
-                                            ? 'tourclassTheme healing'
-                                            : item === '라이프'
-                                            ? 'tourclassTheme life'
-                                            : item === '융합'
-                                            ? 'tourclassTheme fusion'
-                                            : 'tourclassTheme'
-                                    }
-                                >
-                                    {item}
-                                </div>
-                            ))}
-                        </div>
-                        <h4>{title}</h4>
-                        <h5>{description}</h5>
-                        <dl className="period">
-                            <dt>
-                                <i></i>기간
-                            </dt>
-                            <dd>{period}</dd>
-                        </dl>
-                    </article>
-                    <span className="overlay"></span>
+const TourClassItem = ({
+  id,
+  title,
+  theme,
+  description,
+  period,
+  price,
+  img,
+}) => {
+  return (
+    <li>
+      <Link to={`/tourclass/${id}`}>
+        <div className="img-wrap">
+          <img src={img} alt={title} />
+          <article className="tourclassInfo">
+            <div className="tourclassThemes">
+              {(Array.isArray(theme) ? theme : [theme]).map((item, index) => (
+                <div
+                  key={index}
+                  className={
+                    item === "역사"
+                      ? "tourclassTheme history"
+                      : item === "예술"
+                      ? "tourclassTheme art"
+                      : item === "힐링"
+                      ? "tourclassTheme healing"
+                      : item === "라이프"
+                      ? "tourclassTheme life"
+                      : item === "융합"
+                      ? "tourclassTheme fusion"
+                      : item === "만들기"
+                      ? "tourclassTheme making"
+                      : item === "요리하기"
+                      ? "tourclassTheme cooking"
+                      : item === "체험하기"
+                      ? "tourclassTheme experience"
+                      : "tourclassTheme"
+                  }
+                >
+                  {item}
                 </div>
-                <div className="title">
-                    <strong>{title}</strong>
-                    <i>
-                        <FiHeart />
-                    </i>
-                </div>
-            </Link>
-        </li>
-    );
+              ))}
+            </div>
+            <h4>{title}</h4>
+
+            <dl className="period">
+              <dt>
+                <i>
+                  <IoTimeOutline />
+                </i>
+                기간
+              </dt>
+              <dd>{period}</dd>
+            </dl>
+
+            <h5>{description}</h5>
+            <h6>{price.toLocaleString()} 원</h6>
+          </article>
+          <span className="hoverImg">
+            <img src="/images/tourclass/tourclassList_hover.png" alt="" />
+          </span>
+          <span className="overlay"></span>
+        </div>
+        <div className="title">
+          <strong>{title}</strong>
+          <i>
+            <FiHeart />
+          </i>
+        </div>
+      </Link>
+    </li>
+  );
 };
 
 export default TourClassItem;

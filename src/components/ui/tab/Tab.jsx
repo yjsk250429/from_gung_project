608af79feb52ba87114f39662f7def7d434acf12
yjsk250429 <yejin1046@gmail.com>
2025-09-15 11:36:12 +0900

    예진

--- src/components/ui/tab/Tab.jsx
+++ src/components/ui/tab/Tab.jsx
@@ -1,16 +1,19 @@
 import { useState } from 'react';
 import './style.scss';
 
-const Tab = ({ items = [], onClick = () => {}, textColor, borderColor, height }) => {
+const Tab = ({ items = [], onClick = () => {}, textColor, borderColor}) => {
     const [activeTab, setActiveTab] = useState(0);
     const handleclick = (index) =>{
         setActiveTab(index);
         onClick(index);
     };
-
+    const style ={
+      borderColor:borderColor,
+      fontColor:textColor,
+    }
 
   return (
-    <ul className="tabs">
+    <ul className="tabs" style={style}>
       {items.map((text, index) => (
         <li key={index} onClick={() => handleclick(index)} className={activeTab===index? 'on':''}>
           {text}

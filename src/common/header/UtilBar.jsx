efef512cea59c734423dee13a84c88e01848d39a
yjsk250429 <yejin1046@gmail.com>
2025-09-17 20:23:53 +0900

    auth Store 생성, 헤더 연결

--- src/common/header/UtilBar.jsx
+++ src/common/header/UtilBar.jsx
@@ -1,18 +1,45 @@
 import { TbSearch } from "react-icons/tb";
-import { useModalStore } from "../../store";
+import { useAuthStore, useModalStore } from "../../store";
+import { Link } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+
 const UtilBar = () => {
     const { openLogin, openJoin, openReward, closeReward } = useModalStore();
+    const authed = useAuthStore((s) => s.authed);
+    const logout = useAuthStore((s) => s.logout);
+    const [searchOn, setSearchOn] = useState(false);
+    const formRef = useRef(null);
+    useEffect(() => {
+        const handleClickOutside = (e) => {
+          if (formRef.current && !formRef.current.contains(e.target)) {
+            setSearchOn(false);
+          }
+        };
+    
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+          document.removeEventListener("mousedown", handleClickOutside);
+        };
+      }, []);
 
     return (
         <div className="util">
-        <form >
-            <input type="text" /><i><TbSearch /></i>
+        <form ref={formRef} className={ searchOn ? "on" : ""}>
+            <input type="text" onClick={()=>setSearchOn(true)}/><i><TbSearch /></i>
         </form>
         <ul className="mymenu">
+            {
+                authed ? 
+            <>
+            <li onClick={logout}>로그아웃</li>
+            <li><Link to='/mypage'>마이페이지</Link></li>
+            </>
+            :
+            <>
             <li onClick={openLogin}>로그인</li>
-            {/* <li>로그아웃</li> */}
             <li onClick={openJoin}>회원가입</li>
-            {/* <li>마이페이지</li> */}
+            </>
+            }
         </ul>
         <ul className="reward" onMouseEnter={openReward} onMouseLeave={closeReward}>
             <li><img src="/images/coin_w.png" alt="coin" />0</li>

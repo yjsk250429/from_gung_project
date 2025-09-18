1b0f515b51a88a1461a44d8c90c0670e24653d5f
yjsk250429 <yejin1046@gmail.com>
2025-09-18 18:20:17 +0900

    30px-M 추가

--- src/components/ui/modal/EditInfo.jsx
+++ src/components/ui/modal/EditInfo.jsx
@@ -1,56 +1,52 @@
+import { useModalStore } from '../../../store';
+import Button from '../button/Button';
 import './style.scss';
 
 const EditInfo = () => {
+    const { editInfoOpen, closeEditInfo } = useModalStore();
+    if (!editInfoOpen) return null;
     return (
         <div className="modal-overlay">
         <div className="modal editInfo">
             <h3>회원정보 수정</h3>
+            <ul className='id'>
+                <li>아이디</li>
+                <li>abc1234</li>
+            </ul>
+            <ul className='name'>
+                <li>이름</li>
+                <li>홍길동</li>
+            </ul>
+            <ul className='birthday'>
+                <li>생년월일</li>
+                <li>1999.01.01</li>
+            </ul>
 
             <form>
-            <label htmlFor="">
-                이름*
-                <input type="text" />
-            </label>
             <label htmlFor="">
                 닉네임
                 <input type="text" />
             </label>
             <label htmlFor="">
-                아이디*
-                <input type="text" />
-            </label>
-            <label htmlFor="">
-                비밀번호*
+                비밀번호 변경
                 <input type="password" />
             </label>
             <label htmlFor="">
-                비밀번호 확인*
+                비밀번호 변경 확인
                 <input type="password" />
             </label>
             <label htmlFor="">
-                연락처*
+                연락처
                 <input type="text" />
             </label>
             <label htmlFor="">
-                생년월일
-                    <select name="year" id="">
-                        <option value=""></option>
-                        <option value=""></option>
-                        <option value=""></option>
-                    </select>
-                    <select name="month" id="">
-                        <option value=""></option>
-                        <option value=""></option>
-                        <option value=""></option>
-                    </select>
-                    <select name="date" id="">
-                        <option value=""></option>
-                        <option value=""></option>
-                        <option value=""></option>
-                    </select>
+                <input type="checkbox" />
+                <span>(선택)</span> 이벤트·혜택 정보 수신 동의
+                <p>수신거부 2025.09.04 17:32</p>
             </label>
+
                 <p className="btns">
-                    <Button text="취소" className="small white" />
+                    <Button text="취소" className="small white" onClick={closeEditInfo}/>
                     <Button text="저장" className="small white" />
                 </p>
             </form>

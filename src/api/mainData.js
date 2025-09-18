bc7def1ba478c12ad5c4c8936b5859167c68fec1
deaoeon <deaoeon1@gmail.com>
2025-09-18 11:03:18 +0900

    intro 수정

--- src/api/mainData.js
+++ src/api/mainData.js
@@ -1,10 +1,10 @@
 export default [
-    { id: 1, img: '/images/con1_default.png', label: '덕수궁기행', text: 'ㄱ' },
-    { id: 2, img: '/images/con1_default.png', label: '경복궁투어', text: 'ㄴ' },
-    { id: 3, img: '/images/con1_default.png', label: '창덕궁산책', text: 'ㄷ' },
-    { id: 4, img: '/images/con1_default.png', label: '종묘문화재', text: 'ㄹ' },
-    { id: 5, img: '/images/con1_default.png', label: '한옥마을체험', text: 'ㅁ' },
-    { id: 6, img: '/images/con1_default.png', label: '서울야경투어', text: 'ㅂ' },
-    { id: 7, img: '/images/con1_default.png', label: '북촌골목길', text: 'ㅅ' },
-    { id: 8, img: '/images/con1_default.png', label: '전통시장탐방', text: 'ㅇ' },
+    { id: 1, img: '/images/mainbest/con1_default.png', label: '덕수궁기행', text: 'ㄱ' },
+    { id: 2, img: '/images/mainbest/con1_default2.jpg', label: '경복궁투어', text: 'ㄴ' },
+    { id: 3, img: '/images/mainbest/con1_default3.jpg', label: '창덕궁산책', text: 'ㄷ' },
+    { id: 4, img: '/images/mainbest/con1_default.png', label: '종묘문화재', text: 'ㄹ' },
+    { id: 5, img: '/images/mainbest/con1_default.png', label: '한옥마을체험', text: 'ㅁ' },
+    { id: 6, img: '/images/mainbest/con1_default.png', label: '서울야경투어', text: 'ㅂ' },
+    { id: 7, img: '/images/mainbest/con1_default.png', label: '북촌골목길', text: 'ㅅ' },
+    { id: 8, img: '/images/mainbest/con1_default.png', label: '전통시장탐방', text: 'ㅇ' },
 ];
--- src/routes/MyRoutes.jsx
+++ src/routes/MyRoutes.jsx
@@ -2,57 +2,71 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from '../common/Layout';
 
 import {
-  Brand,
-  Home,
-  MyPage,
-  NotFiles,
-  OttDetail,
-  OttMain,
-  OttSearch,
-  OttSub,
-  TourClass,
-  TourClassDetail,
-  TourSearch,
+    Brand,
+    Home,
+    MyPage,
+    NotFiles,
+    OttDetail,
+    OttMain,
+    OttSearch,
+    OttSub,
+    TourClass,
+    TourClassDetail,
+    TourSearch,
 } from '../pages';
 import ScrollToTop from '../components/ui/scrolltotop/ScrollToTop';
 import Intro from '../components/ui/intro/intro';
 
 export const MyRoutes = () => {
-  return (
-    <BrowserRouter>
-      <ScrollToTop/>
-      <Routes>
-        {/* 첫 진입은 Intro 페이지 */}
-        <Route path="/" element={<Intro />} />
+    return (
+        <BrowserRouter>
+            <ScrollToTop />
+            <Routes>
+                {/* 첫 진입은 Intro 페이지 */}
+                <Route path="/" element={<Intro />} />
 
-        {/* 실제 앱은 /home 아래 */}
-        <Route path="/home" element={<Layout />}>
+                {/* 실제 앱은 /home 아래 */}
+                <Route path="/home" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="brand" element={<Brand />} />
+                    <Route path="tourclass" element={<TourClass />} />
+                    <Route path="tourclass/:tourclassID" element={<TourClassDetail />} />
+                    <Route path="tourresults" element={<TourSearch />} />
 
-          <Route index element={<Home />} />
-          <Route path="brand" element={<Brand />} />
-          <Route path="tourclass" element={<TourClass />} />
-          <Route path="tourclass/:tourclassID" element={<TourClassDetail />} />
-          <Route path="tourresults" element={<TourSearch />} />
-          <Route path="ottmain" element={<OttMain />} />
-          <Route path="ottsub" element={<OttSub />} />
-          <Route path="ott/:mediaType/:ottID" element={<OttDetail />} />
-          <Route path="ottresults" element={<OttSearch />} />
-          <Route path="mypage" element={<MyPage />} />
-          <Route path="*" element={<NotFiles />} />
-        </Route>
+                    {/* OTT */}
+                    {/* 
+                        추후 OTT에 다크모드 적용 예정
+                        <Route path="/ott" element={<Layout theme="dark" />} >
+                             <Route index element={<Ott />} />
+                        </Route>
+                        */}
 
-        <Route path="/brand" element={<Navigate to="/home/brand" replace />} />
-        <Route path="/tourclass" element={<Navigate to="/home/tourclass" replace />} />
-        <Route path="/tourclass/:tourclassID" element={<Navigate to="/home/tourclass/:tourclassID" replace />} />
-        <Route path="/tourresults" element={<Navigate to="/home/tourresults" replace />} />
-        <Route path="/ottmain" element={<Navigate to="/home/ottmain" replace />} />
-        <Route path="/ottsub" element={<Navigate to="/home/ottsub" replace />} />
-        <Route path="/ott/:mediaType/:ottID" element={<Navigate to="/home/ott/:mediaType/:ottID" replace />} />
-        <Route path="/ottresults" element={<Navigate to="/home/ottresults" replace />} />
-        <Route path="/mypage" element={<Navigate to="/home/mypage" replace />} />
+                    <Route path="ottmain" element={<OttMain />} />
+                    <Route path="ottsub" element={<OttSub />} />
+                    <Route path="ott/:mediaType/:ottID" element={<OttDetail />} />
+                    <Route path="ottresults" element={<OttSearch />} />
+                    <Route path="mypage" element={<MyPage />} />
+                    <Route path="*" element={<NotFiles />} />
+                </Route>
 
-        <Route path="*" element={<Navigate to="/home" replace />} />
-      </Routes>
-    </BrowserRouter>
-  );
+                <Route path="/brand" element={<Navigate to="/home/brand" replace />} />
+                <Route path="/tourclass" element={<Navigate to="/home/tourclass" replace />} />
+                <Route
+                    path="/tourclass/:tourclassID"
+                    element={<Navigate to="/home/tourclass/:tourclassID" replace />}
+                />
+                <Route path="/tourresults" element={<Navigate to="/home/tourresults" replace />} />
+                <Route path="/ottmain" element={<Navigate to="/home/ottmain" replace />} />
+                <Route path="/ottsub" element={<Navigate to="/home/ottsub" replace />} />
+                <Route
+                    path="/ott/:mediaType/:ottID"
+                    element={<Navigate to="/home/ott/:mediaType/:ottID" replace />}
+                />
+                <Route path="/ottresults" element={<Navigate to="/home/ottresults" replace />} />
+                <Route path="/mypage" element={<Navigate to="/home/mypage" replace />} />
+
+                <Route path="*" element={<Navigate to="/home" replace />} />
+            </Routes>
+        </BrowserRouter>
+    );
 };

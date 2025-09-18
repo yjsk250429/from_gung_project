9691dff580e5c2065ccd420d1cf7f9c9b0315f86
deaoeon <deaoeon1@gmail.com>
2025-09-18 11:16:20 +0900

    0918 app

--- src/routes/MyRoutes.jsx
+++ src/routes/MyRoutes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from '../common/Layout';
 
 import {
@@ -25,13 +25,15 @@ export const MyRoutes = () => {
                 {/* 첫 진입은 Intro 페이지 */}
                 <Route path="/" element={<Intro />} />
 
-                {/* 실제 앱은 /home 아래 */}
-                <Route path="/home" element={<Layout />}>
+                <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
-                    <Route path="brand" element={<Brand />} />
-                    <Route path="tourclass" element={<TourClass />} />
-                    <Route path="tourclass/:tourclassID" element={<TourClassDetail />} />
-                    <Route path="tourresults" element={<TourSearch />} />
+
+                    <Route path="/brand" element={<Brand />} />
+
+                    {/* 투어 */}
+                    <Route path="/tourclass" element={<TourClass />} />
+                    <Route path="/tourclass/:tourclassID" element={<TourClassDetail />} />
+                    <Route path="/tourresults" element={<TourSearch />} />
 
                     {/* OTT */}
                     {/* 
@@ -40,32 +42,18 @@ export const MyRoutes = () => {
                              <Route index element={<Ott />} />
                         </Route>
                         */}
+                    <Route path="/ottmain" element={<OttMain />} />
+                    <Route path="/ottsub" element={<OttSub />} />
 
-                    <Route path="ottmain" element={<OttMain />} />
-                    <Route path="ottsub" element={<OttSub />} />
                     <Route path="ott/:mediaType/:ottID" element={<OttDetail />} />
-                    <Route path="ottresults" element={<OttSearch />} />
-                    <Route path="mypage" element={<MyPage />} />
+                    {/* <Route path="/vod/:medsiaType/:id" element={<OttDetail />} /> */}
+                    {/* <Route path="/ott/ottdetail" element={<OttDetail />} /> */}
+                    <Route path="/ottresults" element={<OttSearch />} />
+
+                    <Route path="/mypage" element={<MyPage />} />
+
                     <Route path="*" element={<NotFiles />} />
                 </Route>
-
-                <Route path="/brand" element={<Navigate to="/home/brand" replace />} />
-                <Route path="/tourclass" element={<Navigate to="/home/tourclass" replace />} />
-                <Route
-                    path="/tourclass/:tourclassID"
-                    element={<Navigate to="/home/tourclass/:tourclassID" replace />}
-                />
-                <Route path="/tourresults" element={<Navigate to="/home/tourresults" replace />} />
-                <Route path="/ottmain" element={<Navigate to="/home/ottmain" replace />} />
-                <Route path="/ottsub" element={<Navigate to="/home/ottsub" replace />} />
-                <Route
-                    path="/ott/:mediaType/:ottID"
-                    element={<Navigate to="/home/ott/:mediaType/:ottID" replace />}
-                />
-                <Route path="/ottresults" element={<Navigate to="/home/ottresults" replace />} />
-                <Route path="/mypage" element={<Navigate to="/home/mypage" replace />} />
-
-                <Route path="*" element={<Navigate to="/home" replace />} />
             </Routes>
         </BrowserRouter>
     );

2062b1540628a9a4b27db2a759e6b4f45c07f8c0
deaoeon <deaoeon1@gmail.com>
2025-09-19 12:35:22 +0900

    OTT 페이지 더보기 추가

--- src/components/ottSub/OttList.jsx
+++ src/components/ottSub/OttList.jsx
@@ -1,21 +1,21 @@
 import './style.scss';
-import { useEffect } from "react";
-import { useMovieStore } from "../../store";
+import { useEffect } from 'react';
+import { useMovieStore } from '../../store';
 import OttItem from './OttItem';
 
 const OttList = () => {
     const { movies, fetchMovies, loading, error } = useMovieStore();
 
     useEffect(() => {
-        fetchMovies({ category: "popular", page: 1 });
-      }, [fetchMovies]);
-    
-      if (loading) return <p>로딩 중...</p>;
-      if (error) return <p>에러 발생: {error}</p>;
+        fetchMovies({ category: 'popular', page: 1 });
+    }, [fetchMovies]);
+
+    if (loading) return <p>로딩 중...</p>;
+    if (error) return <p>에러 발생: {error}</p>;
     return (
-        <ul className='ottlist'>
+        <ul className="ottlist">
             {movies.map((movie) => (
-              <OttItem key={movie.id} {...movie}/>
+                <OttItem key={movie.id} {...movie} />
             ))}
         </ul>
     );

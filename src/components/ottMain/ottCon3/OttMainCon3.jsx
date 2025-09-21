bbef53ef697e84f6c5468d8d525794b518f1010e
burtsbees <kje_4227@naver.com>
2025-09-21 19:02:40 +0900

    KJE_detailCon2

--- src/components/ottMain/ottCon3/OttMainCon3.jsx
+++ src/components/ottMain/ottCon3/OttMainCon3.jsx
@@ -1,152 +1,159 @@
-import './style.scss';
-import { useEffect, useRef } from 'react';
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { CustomEase } from 'gsap/CustomEase';
-import { useNavigate } from 'react-router-dom';
+import "./style.scss";
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { CustomEase } from "gsap/CustomEase";
+import { useNavigate } from "react-router-dom";
 
 gsap.registerPlugin(ScrollTrigger);
-CustomEase.create('sameEase', '0.25, 0.1, 0.25, 1');
+gsap.registerPlugin(CustomEase);
+CustomEase.create("sameEase", "0.25, 0.1, 0.25, 1");
 
 const OttMainCon3 = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const LoopTopRef = useRef(null);
-    useEffect(() => {
-        if (LoopTopRef.current) {
-            gsap.set(LoopTopRef.current, { xPercent: 0 });
+  const LoopTopRef = useRef(null);
+  useEffect(() => {
+    if (LoopTopRef.current) {
+      gsap.set(LoopTopRef.current, { xPercent: 0 });
 
-            gsap.to(LoopTopRef.current, {
-                xPercent: -50,
-                repeat: -1,
-                duration: 40,
-                ease: 'linear',
-            });
+      gsap.to(LoopTopRef.current, {
+        xPercent: -50,
+        repeat: -1,
+        duration: 40,
+        ease: "linear",
+      });
+    }
+  }, []);
+
+  const LoopBottomRef = useRef(null);
+  useEffect(() => {
+    if (LoopBottomRef.current) {
+      gsap.set(LoopBottomRef.current, { xPercent: -50 });
+
+      gsap.to(LoopBottomRef.current, {
+        xPercent: 0,
+        repeat: -1,
+        duration: 40,
+        ease: "linear",
+      });
+    }
+  }, []);
+
+  const pic04Ref = useRef(null);
+  const pic05Ref = useRef(null);
+  useEffect(() => {
+    if (pic04Ref.current) {
+      gsap.fromTo(
+        pic04Ref.current,
+        { x: -150, opacity: 0 },
+        {
+          x: 0,
+          opacity: 1,
+          duration: 0.8,
+          ease: "sameEase",
+          scrollTrigger: {
+            trigger: pic04Ref.current,
+            start: "top 75%",
+            toggleActions: "play none none reverse",
+            // markers: true,
+          },
         }
-    }, []);
+      );
+    }
 
-    const LoopBottomRef = useRef(null);
-    useEffect(() => {
-        if (LoopBottomRef.current) {
-            gsap.set(LoopBottomRef.current, { xPercent: -50 });
-
-            gsap.to(LoopBottomRef.current, {
-                xPercent: 0,
-                repeat: -1,
-                duration: 40,
-                ease: 'linear',
-            });
+    if (pic05Ref.current) {
+      gsap.fromTo(
+        pic05Ref.current,
+        { x: 150, opacity: 0 },
+        {
+          x: 0,
+          opacity: 1,
+          duration: 0.8,
+          ease: "sameEase",
+          scrollTrigger: {
+            trigger: pic05Ref.current,
+            start: "top 90%",
+            toggleActions: "play none none reverse",
+            // markers: true,
+          },
         }
-    }, []);
+      );
+    }
+  }, []);
 
-    const pic04Ref = useRef(null);
-    const pic05Ref = useRef(null);
-    useEffect(() => {
-        if (pic04Ref.current) {
-            gsap.fromTo(
-                pic04Ref.current,
-                { x: -150, opacity: 0 },
-                {
-                    x: 0,
-                    opacity: 1,
-                    duration: 0.8,
-                    ease: 'sameEase',
-                    scrollTrigger: {
-                        trigger: pic04Ref.current,
-                        start: 'top 75%',
-                        toggleActions: 'play none none reverse',
-                        // markers: true,
-                    },
-                }
-            );
-        }
-
-        if (pic05Ref.current) {
-            gsap.fromTo(
-                pic05Ref.current,
-                { x: 150, opacity: 0 },
-                {
-                    x: 0,
-                    opacity: 1,
-                    duration: 0.8,
-                    ease: 'sameEase',
-                    scrollTrigger: {
-                        trigger: pic05Ref.current,
-                        start: 'top 90%',
-                        toggleActions: 'play none none reverse',
-                        // markers: true,
-                    },
-                }
-            );
-        }
-    }, []);
-
-    return (
-        <section className="ottMainCon3">
-            <div className="inner">
-                <section className="ottPosters_Left">
-                    <div className="jagaeImg">
-                        <img src="/images/ott/mainJagae_flower.png" alt="jagae" />
-                    </div>
-                    <div
-                        className="poster pic04"
-                        ref={pic04Ref}
-                        onClick={() => navigate('/ott/tv/135157')}
-                    >
-                        <img
-                            className="jagae"
-                            src="/images/ott/ottPoster_Jagae_750x420.png"
-                            alt="hoverJagaeImg"
-                        />
-                        <article className="posterText">
-                            <h3>[드라마] 환혼</h3>
-                            <h4>이재욱, 정소민, 황민현, 신승호, 유준상, 오나라</h4>
-                            <img src="/images/ott/PosterTextbox_hoverImg.png" alt="hoverTextImg" />
-                            <p>
-                                역사와 지도에 없는 대호국을 배경으로,
-                                <br />
-                                ‘환혼술’로 운명이 비틀린 주인공들이 성장하는 판타지 로맨스 활극
-                            </p>
-                        </article>
-                        <span className="overlay"></span>
-                    </div>
-                </section>
-                <div
-                    className="poster pic05"
-                    ref={pic05Ref}
-                    onClick={() => navigate('/ott/movie/660360')}
-                >
-                    <img
-                        className="jagae"
-                        src="/images/ott/ottPoster_Jagae_750x1060.png"
-                        alt="hoverJagaeImg"
-                    />
-                    <article className="posterText">
-                        <h3>[영화] 노량: 죽음의 바다</h3>
-                        <h4>김윤석, 백윤식, 정재영, 허준호, 김성규, 이규형</h4>
-                        <img src="/images/ott/PosterTextbox_hoverImg.png" alt="hoverTextImg" />
-                        <p>
-                            조선을 지키는 불굴의 명장이 일본과의 해상 전쟁을 끝내기 위해
-                            <br />
-                            명나라 함대와 위험한 공조를 시작한다.
-                            <br />
-                            역사에 길이 남을 마지막 전투의 막이 오른다.
-                        </p>
-                    </article>
-                    <span className="overlay"></span>
-                </div>
-            </div>
-
-            <div className="textLoop_Top" ref={LoopTopRef}>
-                <p>STEP INTO KOREA'S TIMELESS JOURNEY</p>
-                <p>STEP INTO KOREA'S TIMELESS JOURNEY</p>
-            </div>
-            <div className="textLoop_Bottom" ref={LoopBottomRef}>
-                <p>시간을 넘어 한국의 여정 속으로</p>
-                <p>시간을 넘어 한국의 여정 속으로</p>
-            </div>
+  return (
+    <section className="ottMainCon3">
+      <div className="inner">
+        <section className="ottPosters_Left">
+          <div className="jagaeImg">
+            <img src="/images/ott/mainJagae_flower.png" alt="jagae" />
+          </div>
+          <div
+            className="poster pic04"
+            ref={pic04Ref}
+            onClick={() => navigate("/ott/tv/135157")}
+          >
+            <img
+              className="jagae"
+              src="/images/ott/ottPoster_Jagae_750x420.png"
+              alt="hoverJagaeImg"
+            />
+            <article className="posterText">
+              <h3>[드라마] 환혼</h3>
+              <h4>이재욱, 정소민, 황민현, 신승호, 유준상, 오나라</h4>
+              <img
+                src="/images/ott/PosterTextbox_hoverImg.png"
+                alt="hoverTextImg"
+              />
+              <p>
+                역사와 지도에 없는 대호국을 배경으로,
+                <br />
+                ‘환혼술’로 운명이 비틀린 주인공들이 성장하는 판타지 로맨스 활극
+              </p>
+            </article>
+            <span className="overlay"></span>
+          </div>
         </section>
-    );
+        <div
+          className="poster pic05"
+          ref={pic05Ref}
+          onClick={() => navigate("/ott/movie/660360")}
+        >
+          <img
+            className="jagae"
+            src="/images/ott/ottPoster_Jagae_750x1060.png"
+            alt="hoverJagaeImg"
+          />
+          <article className="posterText">
+            <h3>[영화] 노량: 죽음의 바다</h3>
+            <h4>김윤석, 백윤식, 정재영, 허준호, 김성규, 이규형</h4>
+            <img
+              src="/images/ott/PosterTextbox_hoverImg.png"
+              alt="hoverTextImg"
+            />
+            <p>
+              조선을 지키는 불굴의 명장이 일본과의 해상 전쟁을 끝내기 위해
+              <br />
+              명나라 함대와 위험한 공조를 시작한다.
+              <br />
+              역사에 길이 남을 마지막 전투의 막이 오른다.
+            </p>
+          </article>
+          <span className="overlay"></span>
+        </div>
+      </div>
+
+      <div className="textLoop_Top" ref={LoopTopRef}>
+        <p>STEP INTO KOREA'S TIMELESS JOURNEY</p>
+        <p>STEP INTO KOREA'S TIMELESS JOURNEY</p>
+      </div>
+      <div className="textLoop_Bottom" ref={LoopBottomRef}>
+        <p>시간을 넘어 한국의 여정 속으로</p>
+        <p>시간을 넘어 한국의 여정 속으로</p>
+      </div>
+    </section>
+  );
 };
 
 export default OttMainCon3;
--- src/pages/tourclassDetail/index.jsx
+++ src/pages/tourclassDetail/index.jsx
@@ -1,115 +1,129 @@
-import { useParams } from 'react-router-dom';
-import { useTourClassStore } from '../../store';
-import './style.scss';
-import Tab from '../../components/ui/tab/Tab';
-import { MdOutlineCalendarMonth } from 'react-icons/md';
-import { LuClock3, LuMapPin } from 'react-icons/lu';
-import { IoMapOutline } from 'react-icons/io5';
-import { BiCoin } from 'react-icons/bi';
-import { useState } from 'react';
-import { Reviews, Schedule, SelectDate } from '../../components/tourclassDetail';
+import { useParams } from "react-router-dom";
+import { useTourClassStore } from "../../store";
+import "./style.scss";
+import Tab from "../../components/ui/tab/Tab";
+import { MdOutlineCalendarMonth } from "react-icons/md";
+import { LuClock3, LuMapPin } from "react-icons/lu";
+import { IoMapOutline } from "react-icons/io5";
+import { BiCoin } from "react-icons/bi";
+import { useState } from "react";
+import {
+  Reviews,
+  Schedule,
+  SelectDate,
+} from "../../components/tourclassDetail";
 
 const TourClassDatail = () => {
-    const { tourclassID } = useParams();
-    const tourClass = useTourClassStore((state) => state.tourClass);
-    const thisitem = tourClass.find((t) => String(t.id) === tourclassID);
-    const { img, category, title, theme = [], period, price, time, region, place = [] } = thisitem;
-    const items = ['투어 일정', '예약하기', '리뷰'];
-    const [activeTab, setActiveTab] = useState(0);
+  const { tourclassID } = useParams();
+  const tourClass = useTourClassStore((state) => state.tourClass);
+  const thisitem = tourClass.find((t) => String(t.id) === tourclassID);
+  const {
+    img,
+    category,
+    title,
+    theme = [],
+    period,
+    price,
+    time,
+    region,
+    place = [],
+  } = thisitem;
+  const items = ["투어 일정", "예약하기", "리뷰"];
+  const [activeTab, setActiveTab] = useState(0);
 
-    const REWARD_UNIT = 2000;
-    const reward = Math.ceil(price / REWARD_UNIT);
+  const REWARD_UNIT = 2000;
+  const reward = Math.ceil(price / REWARD_UNIT);
 
-    if (!thisitem) {
-        return <p>존재하지 않는 투어/클래스입니다.</p>;
-    }
+  if (!thisitem) {
+    return <p>존재하지 않는 투어/클래스입니다.</p>;
+  }
 
-    return (
-        <section className="tourclassDetail">
-            <div className="inner">
-                <article className="left">
-                    <div className="img-wrap">
-                        <img src={img} alt={title} />
-                    </div>
-                </article>
-                <article className="right">
-                    <ul className="theme">
-                        {theme.map((tm, i) => (
-                            <li
-                                key={i}
-                                className={
-                                    tm === '역사'
-                                        ? 'history'
-                                        : tm === '예술'
-                                        ? 'art'
-                                        : tm === '힐링'
-                                        ? 'healing'
-                                        : tm === '라이프'
-                                        ? 'life'
-                                        : tm === '융합'
-                                        ? 'fusion'
-                                        : tm === '만들기'
-                                        ? 'making'
-                                        : tm === '요리하기'
-                                        ? 'cooking'
-                                        : tm === '체험하기'
-                                        ? 'experience'
-                                        : ''
-                                }
-                            >
-                                {tm}
-                            </li>
-                        ))}
-                    </ul>
-                    <div className="basic-info">
-                        <h3>
-                            {title}
-                            <span className="rate">평점 ★★★★★</span>
-                        </h3>
-                        <ul className="details">
-                            <li>
-                                <i>
-                                    <MdOutlineCalendarMonth />
-                                </i>
-                                {time}
-                            </li>
-                            <li>
-                                <i>
-                                    <LuClock3 />
-                                </i>
-                                {period}
-                            </li>
-                            <li>
-                                <i>
-                                    <IoMapOutline />
-                                </i>
-                                {region}
-                            </li>
-                            <li>
-                                <i>
-                                    <LuMapPin />
-                                </i>
-                                {category === 'tour' ? `${place.length}개 명소` : `${place[0]}`}
-                            </li>
-                            <li>
-                                <i>
-                                    <BiCoin />
-                                </i>
-                                1인 {price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원
-                                <span>(리워드 {reward}p 지급)</span>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="group">
-                        <Tab items={items} activeIndex={activeTab} onClick={setActiveTab} />
-                        {activeTab === 0 && <Schedule thisitem={thisitem} />}
-                        {activeTab === 1 && <SelectDate />}
-                        {activeTab === 2 && <Reviews />}
-                    </div>
-                </article>
-            </div>
-        </section>
-    );
+  return (
+    <section className="tourclassDetail">
+      <div className="inner">
+        <article className="left">
+          <div className="img-wrap">
+            <img src={img} alt={title} />
+          </div>
+        </article>
+        <article className="right">
+          <ul className="theme">
+            {theme.map((tm, i) => (
+              <li
+                key={i}
+                className={
+                  tm === "역사"
+                    ? "history"
+                    : tm === "예술"
+                    ? "art"
+                    : tm === "힐링"
+                    ? "healing"
+                    : tm === "라이프"
+                    ? "life"
+                    : tm === "융합"
+                    ? "fusion"
+                    : tm === "만들기"
+                    ? "making"
+                    : tm === "요리하기"
+                    ? "cooking"
+                    : tm === "체험하기"
+                    ? "experience"
+                    : ""
+                }
+              >
+                {tm}
+              </li>
+            ))}
+          </ul>
+          <div className="basic-info">
+            <h3>
+              {title}
+              <span className="rate">평점 ★★★★★</span>
+            </h3>
+            <ul className="details">
+              <li>
+                <i>
+                  <MdOutlineCalendarMonth />
+                </i>
+                {time}
+              </li>
+              <li>
+                <i>
+                  <LuClock3 />
+                </i>
+                {period}
+              </li>
+              <li>
+                <i>
+                  <IoMapOutline />
+                </i>
+                {region}
+              </li>
+              <li>
+                <i>
+                  <LuMapPin />
+                </i>
+                {category === "tour" ? `${place.length}개 명소` : `${place[0]}`}
+              </li>
+              <li>
+                <i>
+                  <BiCoin />
+                </i>
+                1인 {price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원
+                <span>(리워드 {reward}p 지급)</span>
+              </li>
+            </ul>
+          </div>
+          <div className="group">
+            <Tab items={items} activeIndex={activeTab} onClick={setActiveTab} />
+            {activeTab === 0 && <Schedule thisitem={thisitem} />}
+            {activeTab === 1 && <SelectDate thisitem={thisitem} />}
+            {activeTab === 2 && <Reviews />}
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default TourClassDatail;

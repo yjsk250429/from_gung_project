1b38a195c3e5275c5528676bc9218b069f65fc25
jju <ichie8699@gmail.com>
2025-09-22 19:56:51 +0900

    0922결제하기

--- src/components/booking/Payment.jsx
+++ src/components/booking/Payment.jsx
@@ -3,6 +3,7 @@ import { MdOutlineCalendarMonth } from 'react-icons/md';
 import { LuClock3, LuMapPin } from 'react-icons/lu';
 import { IoMapOutline } from 'react-icons/io5';
 import { BiCoin } from 'react-icons/bi';
+import { IoIosArrowForward } from 'react-icons/io';
 
 const Payment = () => {
     return (
@@ -116,21 +117,65 @@ const Payment = () => {
                             </strong>
                             <div className="selec">
                                 <li>
-                                    <input type="radio" />
+                                    <input type="radio" name="pay" value="card" checked />
                                     신용카드
                                 </li>
                                 <li>
-                                    <input type="radio" />
+                                    <input type="radio" name="pay" value="bank" checked />
                                     계좌이체
                                 </li>
                                 <li>
-                                    <input type="radio" />
+                                    <input type="radio" name="pay" value="cash" checked />
                                     무통장입금
                                 </li>
                             </div>
                         </div>
                     </div>
                 </article>
+                <section className="right">
+                    <div className="pay-info">
+                        <strong>결제 정보</strong>
+                        <p>
+                            판매금액 <em>54,000원</em>
+                        </p>
+                        <p>
+                            판매수량 X1 <em>54,000원</em>
+                        </p>
+                    </div>
+                    <div className="coupon">
+                        <strong>할인쿠폰</strong>
+                        <span>
+                            사용가능 쿠폰 <p>1장</p>
+                        </span>
+                        <div className="cpbox">
+                            <span>리뷰 1,000개 돌파 기념 할인</span>
+                            <p>1,000원 할인</p>
+                        </div>
+                    </div>
+                    <div className="total">
+                        <strong>
+                            최종 결제금액 <p>53,000원</p>
+                        </strong>
+                        <em>(리워드 10p 적립)</em>
+                    </div>
+                    <div className="check">
+                        <div className="all">
+                            <input type="checkbox" /> 전체동의
+                        </div>
+                        <li>
+                            <input type="checkbox" /> 취소규정 동의 (필수)
+                            <IoIosArrowForward />
+                        </li>
+                        <li>
+                            <input type="checkbox" /> 취소 및 환불 정책 동의 (필수)
+                            <IoIosArrowForward />
+                        </li>
+                        <p>
+                            개인정보 3자 제공 동의 안내 <IoIosArrowForward />
+                        </p>
+                    </div>
+                    <button>53,000원 결제하기</button>
+                </section>
             </div>
         </section>
     );

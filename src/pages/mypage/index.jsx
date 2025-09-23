c9321639ca918f02c566d0de764f208a42006ddf
yjsk250429 <yejin1046@gmail.com>
2025-09-23 15:04:22 +0900

    투어클래스 찜 추가, 삭제 모두 가능

--- src/pages/mypage/index.jsx
+++ src/pages/mypage/index.jsx
@@ -2,10 +2,14 @@ import MyPagemain from '../../components/mypage/MyPagemain';
 import EditInfo from '../../components/ui/modal/EditInfo';
 import EditInfoComplete from '../../components/ui/modal/EditInfoComplete';
 import EditPassword from '../../components/ui/modal/EditPassword';
+import WishModal from '../../components/ui/modal/WishModal';
 import WithdrawComplete from '../../components/ui/modal/WithdrawComplete';
 import WithdrawConfirm from '../../components/ui/modal/WithdrawConfirm';
+import { useAuthStore } from '../../store';
 
 const MyPage = () => {
+        const { user } = useAuthStore();  
+    
     return (
         <>
             <MyPagemain />
@@ -14,6 +18,10 @@ const MyPage = () => {
             <EditInfoComplete />
             <WithdrawConfirm />
             <WithdrawComplete />
+            {
+            user.wishlist.length > 0 &&
+            <WishModal className='deleteAll'/>
+            }
         </>
     );
 };
